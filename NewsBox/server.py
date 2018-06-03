2afa3c9e5a7acc1306f4aeea111f843055cf277f
Tarek <tsabra76@gmail.com>
2018-06-03 03:42:29 -0700

    added conditional weather logic

--- NewsBox/server.py
+++ NewsBox/server.py
@@ -4,21 +4,29 @@ import pywapi as weather
 import easygui as gui
 from flask import Flask
 from reboot import rebootModem
+import weatherpics
 
 app = Flask(__name__)
 
 @app.route('/ny')
 def nyweather():
-	NYnoaa_result = weather.get_weather_from_noaa('KJFK')
-	NYweather = str("New York: " + NYnoaa_result['weather'] + " and " + NYnoaa_result['temp_f'] + "F.\n")
-	return NYweather
+    NYnoaa_result = weather.get_weather_from_noaa('KJFK')
+    NYweather = str("<h1 style=\"text-align:center;\">New York: " + NYnoaa_result['weather'] + " and " + NYnoaa_result['temp_f'] + " F.</h1>\n")
+    if NYnoaa_result['weather'] == "Fair":
+        return NYweather + weatherpics.sunny
+    else:
+        return NYweather + weatherpics.cloudy
+
 
 
 @app.route('/sf')
 def sfweather():
     SFnoaa_result = weather.get_weather_from_noaa('KSFO')
-    SFweather = str("San Francisco: " + SFnoaa_result['weather'] + " and " + SFnoaa_result['temp_f'] + "F.")
-    return SFweather
+    SFweather = str("<h1 style=\"text-align:center;\">San Francisco: " + SFnoaa_result['weather'] + " and " + SFnoaa_result['temp_f'] + " F.</h1>\n")
+    if SFnoaa_result['weather'] == "Fair":
+        return SFweather + weatherpics.sunny
+    else:
+        return SFweather + weatherpics.cloudy
 
 @app.route('/rebootmodem')
 def reboot():
---
+++ NewsBox/weatherpics.py
@@ -0,0 +1,2 @@
+sunny = "<img src ='/static/skc.png'>"
+cloudy = "<img src ='/static/clouds.gif'>"
